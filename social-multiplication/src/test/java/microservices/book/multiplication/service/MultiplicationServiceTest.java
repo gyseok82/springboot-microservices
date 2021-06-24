8b4359e0227bf06aa867e64a9968542955c79576
석귀영 <gyseok@bluedigm.com>
2021-06-24 15:48:42 +0900

    chapter 02

---
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
@@ -0,0 +1,40 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultiplicationServiceTest {
+
+    @MockBean
+    private RandomGeneratorService randomGeneratorService;
+
+    @Autowired
+    private MultiplicationService multiplicationService;
+
+    @Test
+    public void createRandomMultiplicationTest() {
+
+        // given
+        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+
+        // when
+        Multiplication multiplication = multiplicationService.createRandomMultiplication();
+
+        // assert
+        assertThat(multiplication.getFactorA());
+        assertThat(multiplication.getFactorB());
+        assertThat(multiplication.getResult());
+    }
+
+}
